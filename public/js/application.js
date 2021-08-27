a4699fee954a81b996dcbc19d29fbc0faa404e3f
Surkov Pavel <surkovv.pavel@gmail.com>
2021-08-27 13:45:31 +0700

    adds hover-event to work-links

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,15 @@
 
 'use strict';
+
+const workLinks = document.querySelectorAll('.our-works__work');
+
+workLinks.forEach(link => {
+  link.onpointerover = evt => {
+    const additionalNote = evt.target.querySelector('.our-works__work-link');
+    additionalNote.classList.remove('our-works__work-link_hidden');
+  };
+  link.onpointerleave = evt => {
+    const additionalNote = evt.target.querySelector('.our-works__work-link');
+    additionalNote.classList.add('our-works__work-link_hidden');
+  }
+})
